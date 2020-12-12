5c445395d6a8f164678508242df711d1f10441b3
xdedss <liuzhuoran_2000@163.com>
2020-12-12 12:13:32 +0800

    integrator optimizations

---
+++ SC_integrator.py
@@ -0,0 +1,193 @@
+
+
+from scipy.integrate import odeint
+import numpy as np
+from dynamics_functions import Dynamics
+#import sympy
+
+import SC_params
+
+
+idx  = [ 14 ]                # end of x (14,1)
+idx += [idx[0] + (14 * 14)]  # end of A (14,14)
+idx += [idx[1] + (14 * 3)]   # end of B (14,3)
+idx += [idx[2] + (14 * 3)]   # end of C (14,3)
+idx += [idx[3] + (14 * 1)]   # end of S (14,1)
+idx += [idx[4] + (14 * 1)]   # end of z (14,1)
+
+class Integrator:
+    
+    def __init__(self, vessel_profile, params_super):
+        self.matrix_functions = Dynamics()
+        self.matrix_functions.set_parameters({
+            'alpha': 1.0 / 9.806 / vessel_profile.isp,
+            'g_I'  : vessel_profile.g,
+            'rTB'  : vessel_profile.r_T_B,
+            'J'    : vessel_profile.J_B_I,
+        })
+        self.dt = 1 / (params_super.K - 1)
+        
+#        x = sympy.MatrixSymbol('x', 14, 1)
+#        Phi_A = sympy.MatrixSymbol('Phi_A', 14, 14)
+#        B_bar = sympy.MatrixSymbol('B_bar', 14, 3)
+#        C_bar = sympy.MatrixSymbol('C_bar', 14, 3)
+#        S_bar = sympy.MatrixSymbol('S_bar', 14, 1)
+#        z_bar = sympy.MatrixSymbol('z_bar', 14, 1)
+#        
+#        t = sympy.Symbol('t')
+#        u_t = sympy.MatrixSymbol('u_t', 3, 1)
+#        u_t1 = sympy.MatrixSymbol('u_t1', 3, 1)
+#        s = sympy.Symbol('s')
+#        
+#        self.ode_dVdt_expr = ode_dVdt_sep(x, Phi_A, B_bar, C_bar, S_bar, z_bar, t, u_t, u_t1, s, self.dt, (self.matrix_functions.A, self.matrix_functions.B, self.matrix_functions.f))
+
+    def solve(self, x_last_k, u_last_k, u_last_k1, s_last):
+
+        V0 = np.zeros( (idx[-1],) )
+        V0[0      : idx[0]] = x_last_k            # X at initial step
+        V0[idx[0] : idx[1]] = np.eye(14).reshape(-1)  # PhiA at initial step
+        
+        #积分 V为xABCSZ拼合向量
+        V = odeint(
+                ode_dVdt,  # dV/dt function
+                V0,        # initial value of the V container vector
+                (0, self.dt),   # integrate over the time width of a step
+                args = (u_last_k, u_last_k1, s_last, self.dt,
+                    (self.matrix_functions.A, self.matrix_functions.B, self.matrix_functions.f) )
+        )
+
+        V = np.array(V)[1, :]
+        
+        #print(V)
+        
+        return (V[idx[0] : idx[1]].reshape((14, 14)),
+            V[idx[1] : idx[2]].reshape((14, 3)),
+            V[idx[2] : idx[3]].reshape((14, 3)),
+            V[idx[3] : idx[4]],
+            V[idx[4] : idx[5]] )
+                        
+
+
+
+
+def ode_dVdt(V, t, u_t, u_t1, sigma, dt, mat_funcs):
+    ''' integrate the problem vector, which is defined as:
+
+        V = [x(14),
+                Phi_A(14x14), B_bar(14x3), C_bar(14x3), S_bar(14), z_bar(14)]
+
+        V has no correlation to v (velocity) except that it contains v inside
+    '''
+    #print('ode_dVdt')
+    A, B, f = mat_funcs
+    
+    dV_dt = np.zeros( (idx[-1],) )
+    #dV_dt.fill(0)
+    
+    alpha = (t / dt)
+    beta = (1 - t / dt)
+
+    # Gather x and u
+    x = V[0 : idx[0]]
+    u = u_t + alpha * (u_t1 - u_t)
+
+    ''' Since PhiA(t) * A(t) = Phi(t_end), PhiA(t) = Phi(t_end) * A.inverse '''
+    Phi      = V[idx[0] : idx[1]].reshape((14, 14))
+    Phi_A_xi = np.linalg.inv(Phi)
+    
+    A_xus = A(x, u, sigma)
+    B_xus = B(x, u, sigma)
+    f_xu = f(x, u)
+    
+    z_t  = - np.matmul(A_xus, x)
+    z_t += - np.matmul(B_xus, u)
+
+    mul_flat = np.matmul(Phi_A_xi, B_xus).reshape(-1)
+    dV_dt[0      : idx[0]] = sigma * f_xu
+    dV_dt[idx[0] : idx[1]] = np.matmul(A_xus, Phi).reshape(-1)
+    dV_dt[idx[1] : idx[2]] = mul_flat * alpha
+    dV_dt[idx[2] : idx[3]] = mul_flat * beta
+    dV_dt[idx[3] : idx[4]] = np.matmul(Phi_A_xi, f_xu)
+    dV_dt[idx[4] : idx[5]] = np.matmul(Phi_A_xi, z_t)
+    
+    #print(dV_dt)
+    return dV_dt
+#
+#def ode_dVdt_sep(x, Phi_A, B_bar, C_bar, S_bar, z_bar, t, u_t, u_t1, sigma, dt, mat_funcs):
+#    A, B, f = mat_funcs
+#    dV_dt = sympy.zeros( idx[-1], 1 )
+#    
+#    alpha = (t / dt)
+#    beta = (1 - t / dt)
+#    u = u_t + alpha * (u_t1 - u_t)
+#    
+#    Phi_A_xi = Phi_A.inv()
+#    
+#    A_xus = A(x, u, sigma)
+#    B_xus = B(x, u, sigma)
+#    f_xu = f(x, u)
+#    
+#    z_t  = - (A_xus * x)
+#    z_t += - (B_xus * u)
+#    
+#    mul_PhiB = Phi_A_xi* B_xus
+#    return (sigma * f_xu, 
+#        A_xus * Phi_A, 
+#        mul_PhiB * alpha, 
+#        mul_PhiB * beta, 
+#        Phi_A_xi * f_xu, 
+#        Phi_A_xi * z_t)
+#
+## sympy 
+#def ode_dVdt_s(V, t, u_t, u_t1, sigma, dt, mat_funcs):
+#    ''' integrate the problem vector, which is defined as:
+#
+#        V = [x(14),
+#                Phi_A(14x14), B_bar(14x3), C_bar(14x3), S_bar(14), z_bar(14)]
+#
+#        V has no correlation to v (velocity) except that it contains v inside
+#    '''
+#    #print('ode_dVdt')
+#    A, B, f = mat_funcs
+#    
+#    dV_dt = sympy.zeros( idx[-1], 1 )
+#    #dV_dt.fill(0)
+#    
+#    alpha = (t / dt)
+#    beta = (1 - t / dt)
+#    #print(V)
+#    # Gather x and u
+#    x = V[0 : idx[0]]
+#    u = u_t + alpha * (u_t1 - u_t)
+#
+#    ''' Since PhiA(t) * A(t) = Phi(t_end), PhiA(t) = Phi(t_end) * A.inverse '''
+#    Phi      = sympy.Matrix(V[idx[0] : idx[1]]).reshape(14, 14)
+#    #Phi_A_xi = Phi.inv()
+#    Phi_A_xi = sympy.MatrixSymbol('Phi_A_xi', 14, 14)
+#    
+#    A_xus = A(x, u, sigma)
+#    B_xus = B(x, u, sigma)
+#    f_xu = f(x, u)
+#    
+#    z_t  = - (A_xus* x)
+#    z_t += - (B_xus* u)
+#    
+#    #print((sigma * f_xu).shape)
+#    #mul_flat = sympy.Matrix(Phi_A_xi* B_xus).reshape(14 * 3, 1)
+#    dV_dt[0      : idx[0], :] = sigma * f_xu
+#    dV_dt[idx[0] : idx[1], :] = sympy.Matrix(A_xus* Phi).reshape(14 * 14, 1)
+#    dV_dt[idx[1] : idx[2], :] = mul_flat * alpha
+#    dV_dt[idx[2] : idx[3], :] = mul_flat * beta
+#    dV_dt[idx[3] : idx[4], :] = (Phi_A_xi* f_xu)
+#    dV_dt[idx[4] : idx[5], :] = (Phi_A_xi* z_t)
+#    
+#    #print(dV_dt)
+#    return dV_dt
+
+
+
+if __name__ == '__main__':
+    #testing
+    #from dynamics_functions_sympy import Dynamics
+    integrator = Integrator(SC_params.VesselProfile.get_default(), SC_params.SuperParams())
+    
--- SC_solver.py
+++ SC_solver.py
@@ -1,35 +1,35 @@
 
 
-from scipy.integrate import odeint
 import numpy as np
 import math
 from time import time
 import pickle, sys
 
 import SC_params
-from dynamics_functions_buffer import Dynamics
+from SC_integrator import Integrator
 
 
 def solve(vessel_profile, vessel_initial, vessel_final, solver_options=None, params_super=None, use_c = False, verbose = False):
-    #default
+    # default params
     if (params_super == None):
         params_super = SC_params.SuperParams()
     if (solver_options == None):
         solver_options = SC_params.SolverOptions()
     
-    #solver
+    # solver
     if (use_c):
         import SC_subproblem_gen as solver
     else:
         import SC_subproblem as solver
     
-    #successive
+    # super params
     K = params_super.K
     dt = 1 / (K - 1)
     iterations = params_super.iterations
     if (verbose):
         print('K=%s, iterations=%s' % (K, iterations))
     
+    # 参数的转换 将飞行器参数计算转化后填入求解器参数
     params = SC_params.Params(K)
     #sparse
     params.m_dry = vessel_profile.m_dry
@@ -57,14 +57,12 @@ def solve(vessel_profile, vessel_initial, vessel_final, solver_options=None, par
     # time of flight guess
     params.s_last = vessel_profile.time_guess
     
-    # get matrix functions
-    matrix_functions = Dynamics()
-    matrix_functions.set_parameters({
-        'alpha': 1.0 / 9.806 / vessel_profile.isp,
-        'g_I'  : vessel_profile.g,
-        'rTB'  : vessel_profile.r_T_B,
-        'J'    : vessel_profile.J_B_I,
-    })
+    # solver options 凸优化目标函数里各部分权重
+    params.w_delta = solver_options.w_delta
+    params.w_nu = solver_options.w_nu
+    params.w_delta_s = solver_options.w_delta_s
+
+    integrator = Integrator(vessel_profile, params_super)
     
     #  initial guess
     for k in range(K):
@@ -85,40 +83,17 @@ def solve(vessel_profile, vessel_initial, vessel_final, solver_options=None, par
         if (verbose):
             print("Iteration", iteration + 1)
 
+        #（1）积分  准备每个时间步的矩阵A B C S z  过程中需要计算积分，耗时较长
         start_time = time()
-        
-        #准备每个时间步的矩阵A B C S z  过程中需要计算积分，耗时较长
         for k in range(0, K - 1):
-
-            # find A_bar, B_bar, C_bar, Sigma_bar = S_bar, and z_bar
-
-            V0 = np.zeros( (idx[-1],) )
-            V0[0      : idx[0]] = params.x_last[:, k]              # X at initial step
-            V0[idx[0] : idx[1]] = np.eye(14).reshape(-1)  # PhiA at initial step
-            
-            #积分 V为xABCSZ拼合向量
-            V = odeint(
-                    ode_dVdt,  # dV/dt function
-                    V0,        # initial value of the V container vector
-                    (0, dt),   # integrate over the time width of a step
-                    args = (params.u_last[:, k], params.u_last[:, k+1], params.s_last, dt,
-                        (matrix_functions.A, matrix_functions.B, matrix_functions.f) )
-            )
-
-            V = np.array(V)[1, :]
-            
-            #print(V)
-
-            params.A[k][:, :] = V[idx[0] : idx[1]].reshape((14, 14))
-            params.B[k][:, :] = V[idx[1] : idx[2]].reshape((14, 3))
-            params.C[k][:, :] = V[idx[2] : idx[3]].reshape((14, 3))
-            params.S[k][:, 0] = V[idx[3] : idx[4]]
-            params.z[k][:, 0] = V[idx[4] : idx[5]]
-        
-        if (verbose): #积分耗时
+            params.A[k][:, :], params.B[k][:, :], params.C[k][:, :], params.S[k][:, 0], params.z[k][:, 0] = \
+                integrator.solve(params.x_last[:, k], params.u_last[:, k], params.u_last[:, k+1], params.s_last)
+        #积分耗时
+        if (verbose): 
             print(time() - start_time, "sec to integrate")
+        
 
-        #改变w_delta
+        # 令w_delta随着迭代数变大
         if solver_options.force_converge:
             if iteration < solver_options.force_converge_start:
                 params.w_delta = solver_options.w_delta
@@ -126,31 +101,24 @@ def solve(vessel_profile, vessel_initial, vessel_final, solver_options=None, par
                 params.w_delta = solver_options.w_delta * solver_options.force_converge_amount # w_delta变大以促进delta收敛
         else:
             params.w_delta = solver_options.w_delta
-
-        params.w_nu = solver_options.w_nu
-        params.w_delta_s = solver_options.w_delta_s
         
-        
+        #（2）求解凸优化子问题
         start_time = time()
-        
-        #solve
         res, x_res, u_res, s_res, nu_res, delta_res, delta_s_res = solver.solve(params, params_super)
-        
-        if (verbose): #凸优化耗时
+        #凸优化耗时
+        if (verbose): 
             print(time() - start_time, "sec to solve")
         
         # 这次迭代的结果作为下一次迭代的输入
         params.x_last = x_res
         params.u_last = u_res
         params.s_last = s_res
-        for k in range(K):
-            #print((params.u_last[:, k]).shape, np.linalg.norm(params.u_last[:, k]))
+        for k in range(K): #计算u的方向的单位向量
             params.u_last_dir[:, k] = params.u_last[:, k] / np.linalg.norm(params.u_last[:, k])
         
         # 收敛情况的评价指标
         delta_norm = np.linalg.norm(delta_res)
         nu_norm = np.linalg.norm(nu_res, ord=1)
-        
         if (verbose):
             print("Flight time:", s_res, end = ' | ')
             print("Delta_norm:", delta_norm, end = ' | ')
@@ -162,56 +130,8 @@ def solve(vessel_profile, vessel_initial, vessel_final, solver_options=None, par
                 print("Converged after", iteration + 1, "iterations!")
             break
     
-    # 状态，控制，tf
+    #返回 状态，控制，tf
     return x_res, u_res, s_res
-
-
-""" The start/end indexes of each variable in the vector V = XABCSz """
-idx  = [ 14 ]                # end of x (14,1)
-idx += [idx[0] + (14 * 14)]  # end of A (14,14)
-idx += [idx[1] + (14 * 3)]   # end of B (14,3)
-idx += [idx[2] + (14 * 3)]   # end of C (14,3)
-idx += [idx[3] + (14 * 1)]   # end of S (14,1)
-idx += [idx[4] + (14 * 1)]   # end of z (14,1)
-
-dV_dt = np.zeros( (idx[-1],) ) #buffer
-def ode_dVdt(V, t, u_t, u_t1, sigma, dt, mat_funcs):
-    ''' integrate the problem vector, which is defined as:
-
-        V = [x(14),
-                Phi_A(14x14), B_bar(14x3), C_bar(14x3), S_bar(14), z_bar(14)]
-
-        V has no correlation to v (velocity) except that it contains v inside
-    '''
-    #print('ode_dVdt')
-    A, B, f = mat_funcs
-    
-    #dV_dt = np.zeros( (idx[-1],) )
-    dV_dt.fill(0)
-    
-    alpha = (t / dt)
-    beta = (1 - t / dt)
-
-    # Gather x and u
-    x = V[0 : idx[0]]
-    u = u_t + alpha * (u_t1 - u_t)
-
-    ''' Since PhiA(t) * A(t) = Phi(t_end), PhiA(t) = Phi(t_end) * A.inverse '''
-    Phi_A_xi = np.linalg.inv(V[idx[0] : idx[1]].reshape((14, 14)))
-    Phi      = V[idx[0] : idx[1]].reshape((14, 14))
-
-    z_t  = - np.matmul(A(x, u, sigma), x)
-    z_t += - np.matmul(B(x, u, sigma), u)
-
-    dV_dt[0      : idx[0]] = sigma * f(x, u)
-    dV_dt[idx[0] : idx[1]] = np.matmul(A(x, u, sigma), Phi).reshape(-1)
-    dV_dt[idx[1] : idx[2]] = np.matmul(Phi_A_xi, B(x, u, sigma)).reshape(-1) * alpha
-    dV_dt[idx[2] : idx[3]] = np.matmul(Phi_A_xi, B(x, u, sigma)).reshape(-1) * beta
-    dV_dt[idx[3] : idx[4]] = np.matmul(Phi_A_xi, f(x, u))
-    dV_dt[idx[4] : idx[5]] = np.matmul(Phi_A_xi, z_t)
-    
-    #print(dV_dt)
-    return dV_dt
 
 
 if __name__ == '__main__':
